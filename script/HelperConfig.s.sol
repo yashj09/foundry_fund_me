031f87c73ec69fb211d30fc712e03469f494b674
yashj09 <yashj8858@gmail.com>
2024-05-07 23:14:22 +0530

    added anvil support

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 import {Script} from "../lib/forge-std/src/Script.sol";
-
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 contract HelperConfig is Script {
     NetworkConfig public activeNetworkConfig;
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_ANSWER = 2000e8;
     constructor() {
         if (block.chainid == 11155111) {
             activeNetworkConfig = getSepoliaEthConfig();
         } else {
-            activeNetworkConfig = getAnvilEthConfig();
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
         }
     }
     struct NetworkConfig {
@@ -20,9 +22,19 @@ contract HelperConfig is Script {
         });
         return sepoliaConfig;
     }
-    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
         vm.startBroadcast();
-
+        MockV3Aggregator priceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_ANSWER
+        );
         vm.stopBroadcast();
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(priceFeed)
+        });
+        return anvilConfig;
     }
 }
